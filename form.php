4a25a7ccb777ba8fa08650604517331c7f2a72bb
mdyasinahmed <yasinahmed7875@gmail.com>
2024-02-05 03:45:50 +0600

    add MySQL connection code

--- form.php
+++ form.php
@@ -6,7 +6,20 @@
     $email = $_POST['email'];
     $number = $_POST['number'];
 
-    // Database Integration
-    
+    // DB 
+	$conn = new mysqli('localhost','root','','student_registration');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into register(fullName, std_id, department, bloodGroup, email, number) values(?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("ssssss", $fullName, $std_id, $department, $bloodGroup, $email, $number);
+		$number = $stmt->execute();
+		echo $execval;
+		echo "Registration Done Successfully!!";
+		$stmt->close();
+		$conn->close();
+	}
+
 
 ?>
