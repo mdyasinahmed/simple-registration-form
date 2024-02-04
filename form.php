dc16d41f0e0e64930eb608697e8e5ae8e71157e0
mdyasinahmed <yasinahmed7875@gmail.com>
2024-02-05 03:18:31 +0600

    update html and add php code

---
+++ form.php
@@ -0,0 +1,12 @@
+<?
+    $fullName = $_POST['fullName'];
+    $std_id = $_POST['std_id'];
+    $department = $_POST['department'];
+    $bloodGroup = $_POST['bloodGroup'];
+    $email = $_POST['email'];
+    $number = $_POST['number'];
+
+    // Database Integration
+    
+
+?>
